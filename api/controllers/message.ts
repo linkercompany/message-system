b6e4ef75e9d98b6442eb0fafe26fc881e4218125
qraxiss <qraxiss@gmail.com>
2023-10-30 14:41:36 +0300

    controller message

---
+++ api/controllers/message.ts
@@ -0,0 +1,35 @@
+import { MessageLogic } from '../../logic/models/message'
+import { checkAccess } from '../../logic/models/user/access'
+
+import { ahandler, formatter as wrapper } from 'backend-helper-kit'
+
+import { Request, Response, NextFunction } from 'express'
+
+const formatter = wrapper(MessageLogic)
+
+type status = {
+    continue: boolean
+    next: boolean
+}
+
+export class MessageController {
+    @ahandler
+    @formatter
+    static async sendMessage(req: Request, res: Response, next: NextFunction): Promise<status | void> {}
+
+    @ahandler
+    @formatter
+    static async updateMessage(req: Request, res: Response, next: NextFunction): Promise<status | void> {}
+
+    @ahandler
+    @formatter
+    static async deleteMessage(req: Request, res: Response, next: NextFunction): Promise<status | void> {}
+
+    @ahandler
+    @formatter
+    static async getMessage(req: Request, res: Response, next: NextFunction): Promise<status | void> {}
+
+    @ahandler
+    @formatter
+    static async getMessages(req: Request, res: Response, next: NextFunction): Promise<status | void> {}
+}
