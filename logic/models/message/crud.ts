1ea9e9cdf02880802555852b71bcc798deadd792
qraxiss <qraxiss@gmail.com>
2023-10-30 14:40:31 +0300

    message logic

---
+++ logic/models/message/crud.ts
@@ -0,0 +1,62 @@
+import * as inputTypes from '../../types/message/input-type'
+import * as outputTypes from '../../types/message/output-type'
+
+import * as inputValidators from '../../validators/message/input-type'
+import * as outputValidators from '../../validators/message/output-type'
+
+import { ErrorHelper, deleteId, avalidator as wrapper } from 'backend-helper-kit'
+import { variables as config } from '../../../config'
+
+import { MessageModel } from '../../../database/models/message'
+
+const avalidator = wrapper(inputValidators, outputValidators, config)
+
+const errorHelper = new ErrorHelper('Message')
+
+export class MessageLogic {
+    @avalidator
+    @deleteId
+    static async sendMessage(params: inputTypes.sendMessage): Promise<outputTypes.sendMessage> {
+        var result = await MessageModel.create(params.body)
+        errorHelper.createError(result)
+
+        result = result.toObject()
+        return !result
+    }
+
+    @avalidator
+    static async updateMessage(params: inputTypes.updateMessage): Promise<outputTypes.updateMessage> {
+        var result = await MessageModel.updateOne(params.query, { $set: params.body })
+        errorHelper.updateError(result)
+
+        return result.modifiedCount > 0
+    }
+
+    @avalidator
+    static async deleteMessage(params: inputTypes.deleteMessage): Promise<outputTypes.deleteMessage> {
+        var result = await MessageModel.deleteOne(params.query)
+        errorHelper.deleteError(result)
+
+        return result.deletedCount > 0
+    }
+
+    @avalidator
+    @deleteId
+    static async getMessage(params: inputTypes.getMessage): Promise<outputTypes.getMessage> {
+        var result = await MessageModel.findOne(params.query)
+        errorHelper.getError(result)
+
+        return result!.toObject()
+    }
+
+    @avalidator
+    @deleteId
+    static async getMessages(params: inputTypes.getMessages): Promise<outputTypes.getMessages> {
+        var result = await MessageModel.find(params.query)
+        errorHelper.getAllError(result)
+
+        return result.map((item) => {
+            return item.toObject()
+        })
+    }
+}
--- logic/models/user/access.ts
+++ logic/models/user/access.ts
@@ -1,13 +1,15 @@
+import { ForbiddenError } from 'backend-helper-kit'
 import * as types from '../../types/user/user'
 
-export function checkAccess(requires?: any, user?: types.user): boolean {
-    if (!requires) {
-        return true
-    }
+export function checkAccess(requires: any, user: types.user): boolean {
+    let tempPermissons = user.permissions
+    requires.forEach((item: any) => {
+        if (tempPermissons[item]) {
+            tempPermissons = tempPermissons[item]
+        } else {
+            throw new ForbiddenError(`You not have ${requires.join('->')} access`)
+        }
+    })
 
-    /*
-        Do something here
-    */
-
-    return false
+    return true
 }
